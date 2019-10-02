6b9314524ee7c58d2ca1f1886283badc01cc9058
Ilana Mohamed <Imohamed@lcs.on.ca>
2019-10-02 11:42:05 -0400

    Comparison logic works with if statement

--- AGuessingGame/ViewController.swift
+++ AGuessingGame/ViewController.swift
@@ -47,7 +47,16 @@ class ViewController: UIViewController {
         let guessNumber = Int(guessText)!
         
         //For testing purposes, what was the guess?
-        print("For testing purposes, the guess made was \(guessNumber)") 
-    }
+        print("For testing purposes, the guess made was \(guessNumber)")
+        
+        //Give the appropriate feedback to user
+        if guessNumber > targetNumber{
+            print("Guess lower next time.")
+            
+        } else if guessNumber < targetNumber {
+            print("Guess higher next time.")
+        } else {
+            print("You are correct!")
+        }
 }
-
+}
