743b894ccac23fbe0d2f084279be36b5513cb1cb
Ilana Mohamed <Imohamed@lcs.on.ca>
2019-10-01 21:22:44 -0400

    Finish speaking code

--- AGuessingGame/ViewController.swift
+++ AGuessingGame/ViewController.swift
@@ -23,9 +23,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         // Runs as soon as view becomes visible to user.
         super.viewDidLoad()
-        // Do any additional setup after loading the view.// Make an object named 'Synthesizer' which is an instance of the
+        // Do any additional setup after loading the view.// Make an object named 'synthesizer' which is an instance of the
         //class 'AVSpeechSynthesizer'
-        let = AVSpeechSynthesizer {}
+        let synthesiser = AVSpeechSynthesizer()
 
         
         
@@ -33,7 +33,10 @@ class ViewController: UIViewController {
         let message = "I'm thinking of a number between 1-100. Guess what it is."
         //Make an object 'utterance' which is an instance of the
         //class 'AVSpeechUtterance'
-        var = AVSpeechUtterance
+        var utterance = AVSpeechUtterance(string: message)
+        
+        //Speak the message
+        synthesiser.speak(utterance)
     }
     
 
